146ed3ef879ead6d976f7fd9d10e189c7be72a91
Samanth Chinta <samanth.chinta@gmail.com>
2022-03-28 16:51:37 +0200

    Explication de demo.py

--- streamlit/demonstration.py
+++ streamlit/demonstration.py
@@ -26,6 +26,7 @@ dictionnaire = {
 
 def demonstration():
 
+    # Titre et sous-titre
     st.header("Démonstration")
     st.write("Nous allons tester notre réseau de neurones de convolution avec des exemples du dataset MIT test : ")
     st.subheader("Sélection de l'ECG")
@@ -47,19 +48,17 @@ def demonstration():
         
         st.subheader("Prédiction du modèle")
 
+        # Bouton pour lancer la prédiction (session state pour ne pas enlever l'output du 1er bouton quand le 2e est cliqué)
         button1 = st.button("Prédire la classe de l'éléctrocardiogramme")
 
         if st.session_state.get('button') != True:
-
             st.session_state['button'] = button1
-
         if st.session_state['button'] == True:
-
-            st.markdown("La classe prédite par le modèle est **{} : {}**." \
-                .format(pred, dictionnaire[pred]))
-
+            # Output du premier bouton
+            st.markdown("La classe prédite par le modèle est **{} : {}**.".format(pred, dictionnaire[pred]))
             st.markdown("Probabilité de la classe prédite :")
 
+            # Message conditionnel en fonction de la proba de la classe prédite
             if pred_proba > 90 :
                 st.success("La probabilité de la classe prédite est de **{}%**!".format(pred_proba))
             else:
@@ -67,9 +66,9 @@ def demonstration():
                     doit être validé par un expert".format(pred_proba))
 
             if st.button('Découvrir la classe réelle'):
-
+                # Output du 2e bouton
                 st.markdown("La classe réelle de l'ECG est **{} : {}**".format(reel, dictionnaire[reel]))
-
+                # Message conditionnel en fonction de si la classe prédite est la bonne ou pas
                 if pred == reel :
                     st.success("Le modèle a prédit la bonne classe!")
                 else:
